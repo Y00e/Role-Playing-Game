b11870d0ddf91b92ec21a1c693595f17c758d84e
Adam <Adam.sturesson@live.se>
2024-09-16 14:34:56 +0200

    implemented a while loop with switch case in main

--- Role-Playing-Game/Role-Playing-Game.cpp
+++ Role-Playing-Game/Role-Playing-Game.cpp
@@ -6,11 +6,54 @@ int main()
 {
     std::cout << "Welcome to roleplaying game!\n";
 
+    std::cout << "Choose a name for your player: ";
+
+    std::string playerName;
+
+    std::cin >> playerName;
+
+    Player player1 = Player(playerName);
+
+    std::cout <<"Hello " << playerName << ", what do you want to do? \n 1. add item for your inventory \n"
+    "2. show all items in your inventory \n 3. use a item \n 4. remove a item for your inventory \n 5. exit the program";
+
+    int option;
+
+    std::cin >> option;
     Armor chainmail = Armor("Chainmail", 10, 5);
-    Player playerA = Player("A");
-    playerA.addItem(&chainmail);
-    playerA.displayInventory();
-    //kommentar
+
+    int chooseOption;
+
+    bool gameOver;
+
+    while (!gameOver){
+
+        switch(option) {
+            case 1 : player1.addItem(&chainmail);
+            break;
+            
+            case 2 : player1.displayInventory();
+            break;
+            
+            case 3 : 
+            std::cout << "What index do you want to use" << std::endl;
+            std::cin >> chooseOption;
+        
+            player1.useItem(chooseOption);
+            break;
+            
+            case 4 : 
+            std::cout << "What index do you want to delete" << std::endl;
+            std::cin >> chooseOption;
+            player1.removeItem(chooseOption);
+            break;
+            
+            case 5 : gameOver = true;
+            break;
+        };
+
+    };
+
 
     return 0;
 }
