b29ec2f813cba7da1a53cd6f162e0986ff87ca09
inoati1991@gmail.com <inoati1991@gmail.com>
2024-09-16 12:01:28 +0200

    Added potition

---
+++ Potion.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Item.h"
+#include <string>
+
+class Potion : public Item {
+private:
+    int healingAmount; 
+
+public:
+    Potion(const std::string& name, int weight, int healingAmount);
+    void use() override;
+
+    ~Potion();
+};
